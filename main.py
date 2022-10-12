5ebcab54e800f63db016cd64668f9aa4b0021f81
nicola <s1081898@studenti.univpm.it>
2022-10-12 09:37:22 +0200

    Update gui

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from download import download, unzip 
 
 def main():
-    download()
-    unzip()
+    while(True):
+        print("-----------------------------MENU-------------------------------")
+        print("choose the task to be performed by pressing the associated key :")
+        print("d) download file GARR")
+        print("u) unzip and create dataset GARR")
+        print("c) clean dataset GARR")
+        print("e) exit")
+        print("\nSpecial thanks for the layout to Aldo Drago Franconi")
+        print("----------------------------------------------------------------")
+        print("\nYour choice: ")
+        key=input()
+        if key=="d":
+            download()
+        elif key=="u":
+            unzip()
+        elif key=="c":
+            pass
+        elif key=="e":
+            break
+        else:
+            print("wrong key...retry")
 
 if __name__ == "__main__":
     main()
